0aedad498ca0aa407c7f1ecde6095f038a3cbe79
GLira4k <103190663+GLira4k@users.noreply.github.com>
2022-10-20 12:13:55 -0300

    Aula 7 (Prof. Luiz Otavio)

---
+++ aula-7/index.js
@@ -0,0 +1,21 @@
+const nome = 'Gabriel';
+
+console.log(nome);
+
+
+// Não podemos criar constantes com palavras reservadas
+
+// Contantes precisam ter nomes significativos
+
+// Não pode começar um nome de uma contante com número
+
+// Constantes não podem conter espaços ou traços
+
+// Utilizamos camelCase
+
+// Case-sensetive, a letra maiúscula e minúscula fazem diferença
+
+// Não pode mondificar o valor de uma constante
+
+// NÃO UTILIZE "VAR", UTILIZE "CONST"!!
+
