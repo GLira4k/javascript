f3417472d17973c61b0048f7686f516fc0301d58
GLira4k <103190663+GLira4k@users.noreply.github.com>
2022-10-24 11:44:54 -0300

    Update Aula 11

--- aula-11/js/script.js
+++ aula-11/js/script.js
@@ -1,1 +1,14 @@
-alert ('Com a nossa mensagem');
+let num1 = prompt('Digite um número:');
+num1 = Number(num1);
+
+let num2 = prompt('Digite outro número:');
+num2 = Number(num2);
+
+const soma = num1 + num2
+
+alert(`A soma dos dois números é ${soma}`);
+
+
+
+
+
