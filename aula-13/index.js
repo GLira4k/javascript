f5c21aaee4b768c5deed62e47f2678ccfc9eabd1
GLira4k <103190663+GLira4k@users.noreply.github.com>
2022-10-24 17:38:46 -0300

    Aula 12 e 13

---
+++ aula-13/index.js
@@ -0,0 +1,16 @@
+// let umaString = "Um \"texto\"";
+
+//Indice         01234567
+let umaString = "Um texto";
+
+console.log(umaString[4]);
+
+console.log(umaString.charAt(6));
+
+console.log(umaString.concat(' em um lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);
+
+
+
+
