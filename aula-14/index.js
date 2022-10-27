278022c98b0a8d1bdc36dc74604c7cc795895baf
GLira4k <gxavierlira@gmail.com>
2022-10-26 22:40:16 -0300

    Aula 14

---
+++ aula-14/index.js
@@ -0,0 +1,8 @@
+let num1 = 10500; // Number
+let num2 = 2.3; // Number
+
+console.log(num1.toString() + num2); // Para transformar um number em string temporáriamente
+
+//num1 =  num1.toString(); Para transformar um number em string 
+
+console.log(num1.toString(2)); // Para transformar um number em representação binária 
