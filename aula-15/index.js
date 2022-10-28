5ee69ddd33469f5d0fd0f23a55c57921d9cc8d4b
GLira4k <gxavierlira@gmail.com>
2022-10-27 22:50:18 -0300

    Aula 15

---
+++ aula-15/index.js
@@ -0,0 +1,37 @@
+let num1 = 9.5547;
+
+let num2 = Math.floor(num1); // Para arredondar para baixo
+
+console.log(num2); 
+
+let num3 = 6.5234;
+
+let num4 = Math.ceil(num3); // Para arredondar para cima
+
+console.log(num4);
+
+let num5 = 7.6594;
+
+let num6 = Math.round(num5); // Para arredondar para o número mais próximo (0.50 para cima e 0.49 para baixo)
+
+console.log(Math.max(1231, 5455, 8945, 122, 12, 6554)); // Para exibir o maior número da sequência
+
+console.log(Math.min(1231, 5455, 8945, 122, 12, 6554)); // Para exibir o menor número da sequência
+
+const aleatorio = Math.random() // Para gerar um número aleatório entre 0 e 1
+
+console.log(aleatorio); 
+
+const aleatorio2 = Math.round (Math.random() * (10 - 5) + 5);
+
+console.log(aleatorio2);
+
+console.log(Math.PI); // Para exibir valor de PI
+
+console.log(Math.pow(2, 8)) // Para realizar ponteciação
+
+let num7 = 9;
+
+console.log(num7 ** (1/2)); // Para desconbrir a raiz quadrada
+
+console.log(num7 ** 0.5);
