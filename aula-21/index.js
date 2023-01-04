dfeef1a5df90856d4ebf9db5a610286e2a6c614a
GLira4k <103190663+GLira4k@users.noreply.github.com>
2023-01-04 17:40:21 -0300

    Create index.js

---
+++ aula-21/index.js
@@ -0,0 +1,14 @@
+/*Operadores de Comparação
+
+> - Maior que
+>= - Maior que ou igual a 
+< - Menor que
+<= - Menor que ou igual a
+== - Igualdade (Checa valor)
+=== - Igualdade estrita (Checa valor e tipo)
+!= - Diferente (Checa valor)
+!== - Diferente estrito (Checa valor e tipo)
+
+*/
+
+
