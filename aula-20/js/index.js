7a4ed65c5a46ff11aca23d35ec2605d6bfd68c87
GLira4k <103190663+GLira4k@users.noreply.github.com>
2023-01-04 16:35:09 -0300

    Fim da aula 20

--- aula-20/js/index.js
+++ aula-20/js/index.js
@@ -1,9 +1,33 @@
 //IIFE = Immediately Invoked Function Expression
 
-function meuEscopo (){
-    const form = document.querySelector('#form');
+function meuEscopo () {
+    const form = document.querySelector('#form'); // Selecionando uma tag, id ou class no html (.form (para Class) form (para tag))
+    const resultado = document.querySelector('.resultado'); 
 
+    const pessoas = [] //Array vazio para quando clicar em "enviar" o valor preenchido nos campos ser enviado para cá
+    
+    function recebeEventoForm (evento){
+        evento.preventDefault();
+        
+        const nome = form.querySelector('#inome');
+        const sobrenome = form.querySelector('#isobrenome');
+        const peso = form.querySelector('#ipeso');
+        const altura = form.querySelector('#ialtura');
+        
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
 
-                
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    }
+        
+    form.addEventListener('submit', recebeEventoForm)
 }
+
+meuEscopo();
 
